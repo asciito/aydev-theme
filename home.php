d568a07bd3b1fba691c7319702799cdc4873be36
asciito <ayaxdev.mx@gmail.com>
2022-08-16 13:19:12 -0500

    💄 I fix typography for headings

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
-    <h1 class="text-center">Home</h1>
+    <h1 class="text-center pt-5 pb-10">Aydev's Blog</h1>
 
     <?php get_template_part( 'partials/loop', 'entries' ); ?>
 <?php get_footer() ?>
